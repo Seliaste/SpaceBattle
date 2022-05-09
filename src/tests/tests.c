7379bda168e681d15bbcfbdf0d34e51da9539e99
Thomas Wieczorek <thomas.wieczorek1@etu.univ-lorraine.fr>
2022-05-09 15:56:16 +0200

    "fixed" tests

--- src/tests/tests.c
+++ src/tests/tests.c
@@ -7,19 +7,19 @@
  */
 
 #include "../game_logic.h"
-void test_init_sprite_param(sprite_t *sprite, int x, int y, int w, int h, int v, int visible)
+void test_init_sprite_param(sprite_t *sprite, int x, int y, int w, int h, int vv, int vh, int visible)
 {
-    init_sprite(sprite, x, y, w, h, v, visible);
+    init_sprite(sprite, x, y, w, h, vv, vh, visible);
     print_sprite(sprite);
 }
 void test_init_sprite()
 {
     world_t world;
     init_data(&world);
-    test_init_sprite_param(&world.missile, 500, 15, 28, 440, 12, 1);
-    test_init_sprite_param(&world.missile, 100, 10, 10, 2, 4, 1);
-    test_init_sprite_param(&world.missile, 500, 140, 280, 15, 25, 1);
-    test_init_sprite_param(&world.missile, 0, 0, 0, 0, 0, 1);
+    test_init_sprite_param(&world.missile, 500, 15, 28, 440, 5,12, 1);
+    test_init_sprite_param(&world.missile, 100, 10, 10, 2, 4, 8, 1);
+    test_init_sprite_param(&world.missile, 500, 140, 280, 15, 12, 25, 1);
+    test_init_sprite_param(&world.missile, 0, 0, 0, 0, 0, 0, 1);
     // test_init_sprite_param(&world.enemy, 200, SCREEN_HEIGHT + 20, 50, 28, 14, 1);
 }
 void test_ship_limit_param(sprite_t *sprite, world_t *world, int newx)
